093af5f3c40e341666b8d55ff20cd1dce38ff126
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-05-16 20:09:19 +0600

    Task done

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,35 @@ import './App.css';
 import Lotto from "./Lotto/Lotto";
 
 const App = () => {
-    const[circle, setCircle] = useState([
-        {number: 20}
-    ]);
+
+    const [circle, setCircle] = useState([0, 0, 0, 0, 0]);
 
     const changeNumber = () => {
-        setCircle([
-            {number: 36}
-        ])
+        let array: number[] = [];
+        do {
+            let result = Math.floor(Math.random() * 32 + 5);
+            if (!array.includes(result)) {
+                array.push(result);
+                array.sort((a, b) => a - b);
+            }
+        } while (array.length < 5);
+
+        setCircle(array);
     };
 
-      return (
-          <div className="App">
-              <div>
-                  <button className="btn" onClick={changeNumber}>New numbers</button>
-              </div>
 
-                <Lotto number={circle[0].number} />
-                <Lotto number={circle[0].number} />
-                <Lotto number={circle[0].number} />
-                <Lotto number={circle[0].number} />
-                <Lotto number={circle[0].number} />
-          </div>
-      );
+    return (
+        <div className="App">
+            <div>
+                <button className="btn" onClick={changeNumber}>New numbers</button>
+            </div>
+
+            <Lotto number={circle[0]}/>
+            <Lotto number={circle[1]}/>
+            <Lotto number={circle[2]}/>
+            <Lotto number={circle[3]}/>
+            <Lotto number={circle[4]}/>
+        </div>
+    );
 };
-
-export default App;
+    export default App;
